f98e4b2fe4e4fb9dd5efb751b762f6b39e5002a6
Nick Fialkowski <nickolai.fialkowski@gmail.com>
2019-03-01 18:10:13 -0500

    Initial commint
    Empty single-view app template

---
+++ InteractiveStory/InteractiveStory/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InteractiveStory
+//
+//  Created by nikko444 on 2019-03-01.
+//  Copyright © 2019 nikko444. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
