6f9305aa0b090f16d07aebc32b152e1049a4820d
Nick Fialkowski <nickolai.fialkowski@gmail.com>
2019-03-02 13:06:51 -0500

    changer all the properties to lazy var with anonymus method asigning

--- InteractiveStory/InteractiveStory/PageController.swift
+++ InteractiveStory/InteractiveStory/PageController.swift
@@ -8,16 +8,80 @@
 
 import UIKit
 
+extension NSAttributedString {
+    var stringRange: NSRange {
+        return NSMakeRange(0, self.length)
+    }
+}
+
+extension Story {
+    var attributedText: NSAttributedString {
+        let attributedString = NSMutableAttributedString(string: self.text)
+        let paragaphStyle = NSMutableParagraphStyle()
+        paragaphStyle.lineSpacing = 10.0
+        
+        attributedString.addAttribute(NSAttributedString.Key.paragraphStyle, value: paragaphStyle, range: attributedString.stringRange)
+        return attributedString
+    }
+    
+}
+
+extension Page {
+    func story(attributed: Bool) -> NSAttributedString {
+        if attributed == true {
+            return story.attributedText
+        } else {
+            return NSAttributedString(string: story.text)
+        }
+    }
+}
+
 class PageController: UIViewController {
     
     var page: Page? // Here's a right way of initializing a stored property inside of a viewController
     
     //MARK: - UI Properties
     
-    let artworkView = UIImageView()
-    let storyLabel = UILabel()
-    let firstChoiceButton = UIButton(type: .system)
-    let secondChoiceButton = UIButton(type: .system)
+    lazy var artworkView: UIImageView = {  //Anonymus func declaration with LAZY. lazy is initialized only when it's actually used
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false //turns off the auto constraints
+        imageView.image = self.page?.story.artwork
+        return imageView
+    }()
+    
+    lazy var storyLabel: UILabel = { //Anonymus function declaration
+        let label = UILabel()
+        label.numberOfLines = 0
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.attributedText = self.page?.story(attributed: true)
+        return label
+    }()
+    
+    lazy var firstChoiceButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.attributedTitle(for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        
+        let title = self.page?.firstChoice?.title ?? "Play Again"
+        let selector = self.page?.firstChoice != nil ? #selector(PageController.loadFirstChoice) : #selector(PageController.playAgain)
+        
+        button.setTitle(title, for: .normal)
+        button.addTarget(self, action: selector, for: .touchUpInside)
+        
+        return button
+    }()
+    
+    lazy var secondChoiceButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        
+        button.setTitle(self.page?.secondChoice?.title, for: .normal)
+        button.addTarget(self, action: #selector(PageController.loadSecondChoice), for: .touchUpInside)
+        
+        return button
+    }()
+    
+    
     
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
@@ -30,33 +94,12 @@ class PageController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        if let page = page {
-            artworkView.image = page.story.artwork
-            
-            let attributedString = NSMutableAttributedString(string: page.story.text)
-            let paragaphStyle = NSMutableParagraphStyle()
-            paragaphStyle.lineSpacing = 10.0
-            
-            attributedString.addAttribute(NSAttributedString.Key.paragraphStyle, value: paragaphStyle, range: NSMakeRange(0, attributedString.length))
-            
-            storyLabel.attributedText = attributedString
-            
-            if let firstChoice = page.firstChoice {
-                firstChoiceButton.setTitle(firstChoice.title, for: .normal)
-            } else {
-                firstChoiceButton.setTitle("Play Again", for: .normal)
-            }
-            
-            if let secondChoice = page.secondChoice {
-                secondChoiceButton.setTitle(secondChoice.title, for: .normal)
-            }
-        }
+        view.backgroundColor = .white
     }
     
     override func viewDidLayoutSubviews() { //NESTING AN IMAGE VIEW INTO A VIEWCONTROLLER
         super.viewWillLayoutSubviews()
         view.addSubview(artworkView)
-        artworkView.translatesAutoresizingMaskIntoConstraints = false //turns off the auto-resizing thing
         NSLayoutConstraint.activate([
             artworkView.topAnchor.constraint(equalTo: view.topAnchor),
             artworkView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
@@ -65,8 +108,6 @@ class PageController: UIViewController {
         ])
         
         view.addSubview(storyLabel)
-        storyLabel.numberOfLines = 0
-        storyLabel.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
             storyLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16.0),
             storyLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16.0),
@@ -74,18 +115,38 @@ class PageController: UIViewController {
         ])
         
         view.addSubview(firstChoiceButton)
-        firstChoiceButton.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
             firstChoiceButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             firstChoiceButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -80.0)
         ])
         
         view.addSubview(secondChoiceButton)
-        secondChoiceButton.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
             secondChoiceButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             secondChoiceButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -32.0)
         ])
+    }
+    
+    @objc func loadFirstChoice() {
+        if let page = page, let firstChoice = page.firstChoice {
+            let nextPage = firstChoice.page
+            let pageController = PageController(page: nextPage)
+            
+            navigationController?.pushViewController(pageController, animated: true)
+        }
+    }
+    
+    @objc func loadSecondChoice() {
+        if let page = page, let secondChoice = page.secondChoice {
+            let nextPage = secondChoice.page
+            let pageController = PageController(page: nextPage)
+            
+            navigationController?.pushViewController(pageController, animated: true)
+        }
+    }
+    
+    @objc func playAgain () {
+        navigationController?.popToRootViewController(animated: true)
     }
 
 
