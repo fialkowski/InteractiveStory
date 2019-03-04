cc0471fc7199689a10627e3cbd7a1129573f9b9f
Nick Fialkowski <nickolai.fialkowski@gmail.com>
2019-03-04 07:28:23 -0500

    Final working version

--- InteractiveStory/InteractiveStory/PageController.swift
+++ InteractiveStory/InteractiveStory/PageController.swift
@@ -39,6 +39,7 @@ extension Page {
 class PageController: UIViewController {
     
     var page: Page? // Here's a right way of initializing a stored property inside of a viewController
+    let soundEffectsPlayer = SoundEffectsPlayer()
     
     //MARK: - UI Properties
     
@@ -131,7 +132,7 @@ class PageController: UIViewController {
         if let page = page, let firstChoice = page.firstChoice {
             let nextPage = firstChoice.page
             let pageController = PageController(page: nextPage)
-            
+            soundEffectsPlayer.playSound(for: firstChoice.page.story)
             navigationController?.pushViewController(pageController, animated: true)
         }
     }
@@ -140,7 +141,7 @@ class PageController: UIViewController {
         if let page = page, let secondChoice = page.secondChoice {
             let nextPage = secondChoice.page
             let pageController = PageController(page: nextPage)
-            
+            soundEffectsPlayer.playSound(for: secondChoice.page.story)
             navigationController?.pushViewController(pageController, animated: true)
         }
     }
--- InteractiveStory/InteractiveStory/ViewController.swift
+++ InteractiveStory/InteractiveStory/ViewController.swift
@@ -11,9 +11,12 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var textFieldBottomConstraint: NSLayoutConstraint!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        NotificationCenter.default.addObserver(self, selector: #selector(ViewController.keyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(ViewController.keyboardWillHide(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
@@ -39,8 +42,36 @@ class ViewController: UIViewController {
             } catch let error {
                 fatalError("\(error.localizedDescription)")
             }
+        }
+    }
+    
+    @objc func keyboardWillShow(_ notification:  Notification) {
+        if let info = notification.userInfo, let keyboardFrame = info[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue {
+            let frame = keyboardFrame.cgRectValue
+            textFieldBottomConstraint.constant = frame.size.height + 10
+            UIView.animate(withDuration: 0.8) {
+                self.view.layoutIfNeeded()
             }
         }
     }
+    
+    @objc func keyboardWillHide (_ notification:  Notification) {
+        textFieldBottomConstraint.constant = 40
+        
+        UIView.animate(withDuration: 0.8) {
+            self.view.layoutIfNeeded()
+        }
+    }
+    
+    deinit {
+        NotificationCenter.default.removeObserver(self)
+    }
+}
 
 
+extension ViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+}
